6e41fd0f3f607ac0b539944883e6e96bb4b56892
mrspoy <denis@devvela.com>
2014-10-26 15:26:41 +0500

    Added Linux and Windows checkers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString os("Unsupported");
+
+#ifdef Q_OS_WIN32
+    os = "Windows";
+#elif defined(Q_OS_LINUX)
+    os = "Linux";
+#endif
+
+   ui->label->setText("OS is a " + os);
 }
 
 MainWindow::~MainWindow()
