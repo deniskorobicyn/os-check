93e37cdf9a269b0e19fdb2f42e5c69631696b037
deniskorobicyn <denis@devvela.com>
2014-10-26 15:53:31 +0500

    Fixed widows versions
    tested on windows 7

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,11 +13,11 @@ MainWindow::MainWindow(QWidget *parent) :
 #ifdef Q_OS_WIN
     switch(QSysInfo::windowsVersion())
     {
-      case QSysInfo::WV_2000: os = "Windows 2000";
-      case QSysInfo::WV_XP: os = "Windows XP";
-      case QSysInfo::WV_VISTA: os = "Windows Vista";
-      case QSysInfo::WV_WINDOWS7: os = "Windows 7";
-      case QSysInfo::WV_WINDOWS7: os = "Windows 8";
+      case QSysInfo::WV_2000: os = "Windows 2000"; break;
+      case QSysInfo::WV_XP: os = "Windows XP"; break;
+      case QSysInfo::WV_VISTA: os = "Windows Vista"; break;
+      case QSysInfo::WV_WINDOWS7: os = "Windows 7"; break;
+      case QSysInfo::WV_WINDOWS8: os = "Windows 8"; break;
       default: os = "Windows";
     }
 #elif defined(Q_OS_LINUX)
