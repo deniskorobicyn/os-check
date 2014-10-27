6ee6e49789401b40316833bac4abc732eb575f5c
deniskorobicyn <admin@admins-Mac.local>
2014-10-27 13:01:20 -0700

    Fixed other OS
    Tested on Mac OS Mavericks

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QProcess>
+#include <QMessageBox>
 #include <iostream>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
